4c77a290706d6607b3ec136af2bf235b7a6804d0
Zachary Sanders <zsanders@icloud.com>
2017-10-22 10:19:35 -0600

    initialize repo to learn express and mongodb

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+config = require('./server/configure')
+let app = express()
+
+app.set('port', process.env.PORT || 3300);
+app.set('Views', `${__dirname}/Views`)
+app = config(app)
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+app.listen(app.get('port'), () => {
+    console.log(`Server up: http://localhost:${app.get('port')}`);
+})
